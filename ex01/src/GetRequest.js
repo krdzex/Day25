69920bc29a99fa27fe9ad2d967e0aa62966a605c
Krsto Kostic <kostic.krsto@gmail.com>
2021-04-01 21:07:15 +0200

    Adding day25

---
+++ ex01/src/GetRequest.js
@@ -0,0 +1,20 @@
+import React from 'react';
+class GetRequest extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { totalReactPackages: null };
+    }
+    render() {
+        const { totalReactPackages } = this.state;
+        return (
+            <div className="card text-center m-3">
+                <h5 className="card-header">Simple GET Request</h5>
+                <div className="card-body">Total packages: {totalReactPackages}</div>
+            </div>
+        );
+    }
+    componentDidMount() {
+        fetch('https://api.npms.io/v2/search?q=react').then(response => response.json()).then(data => this.setState({ totalReactPackages: data.total }));
+    }
+}
+export default GetRequest;
